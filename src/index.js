d212da59b0c6445cbb311253b56b3e432b72cd67
Thien_Nguyen <thanhc2bg@gmail.com>
2026-07-28 22:08:07 +0700

    Chỉnh lại dự án theo mô hình MVC

--- src/index.js
+++ src/index.js
@@ -4,27 +4,27 @@ const app = express();
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const port = 3000;
-
+const route = require('./routes');
+// cấu hình file static
 app.use(express.static(path.join(__dirname, 'public')));
-
+//tích hợp middleware
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 // bootstrap 5
 app.use('/bootstrap', express.static(path.join(__dirname, '..', 'node_modules', 'bootstrap', 'dist')));
 
 // HTTP logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 // Handlebars
 app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname,'resources\\views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
